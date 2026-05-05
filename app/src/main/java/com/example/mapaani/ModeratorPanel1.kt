49358bcd8cc3e5de857c33a2e48798e9c687a12a
Vike14 <markrovic.dato@gmail.com>
2026-05-05 20:01:50 +0800

    The Three panels for moderator side

---
+++ app/src/main/java/com/example/mapaani/ModeratorPanel1.kt
@@ -0,0 +1,20 @@
+package com.example.mapaani
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ModeratorPanel1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_moderator_panel1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
